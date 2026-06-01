3343f87d28343f81c45523b99bf80e471c8a9f3d
bigmouse <cuixu0928@gmail.com>
2026-06-01 14:49:09 +0800

    Update run_test_ddp.sh

--- run_test_ddp.sh
+++ run_test_ddp.sh
@@ -1,21 +1,20 @@
-# export WANDB_MODE=disabled
 export CUDA_LAUNCH_BLOCKING=1
 export CUDA_VISIBLE_DEVICES=0,1,2,3
 
-DATASET=Instruments
-DATA_PATH=../data
-OUTPUT_DIR=./ckpt/$DATASET/
-RESULTS_FILE=./results/$DATASET/ddp.json
-BASE_MODEL= # LLaMA
+DATASET=Instruments_copytwo
+DATA_PATH=./data
+OUTPUT_DIR=./ckpt_Our/$DATASET/
+RESULTS_FILE=./results/$DATASET/our2_1.json
+BASE_MODEL="LLM path"
+CKPT_PATH=./ckpt_Our/$DATASET/
 
 torchrun --nproc_per_node=4 --master_port=4324 test_ddp.py \
-    --ckpt_path  \
+    --ckpt_path $CKPT_PATH \
     --base_model $BASE_MODEL\
     --dataset $DATASET \
     --data_path $DATA_PATH \
     --results_file $RESULTS_FILE \
     --test_batch_size 1 \
-    --num_beams 20 \
+    --num_beams 8 \
     --test_prompt_ids 0 \
-    --index_file .index.json
-
+    --index_file .item_sid_output.json
