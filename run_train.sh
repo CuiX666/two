5f09e115819e6e93941f1171b267bb9d183f4c2d
bigmouse <cuixu0928@gmail.com>
2026-06-01 14:48:24 +0800

    Update run_train.sh

--- run_train.sh
+++ run_train.sh
@@ -1,25 +1,19 @@
-# export WANDB_MODE=disabled
-export CUDA_LAUNCH_BLOCKING=1
-export CUDA_VISIBLE_DEVICES=0,1,2,3
-
-DATASET=Instruments
-BASE_MODEL= # LLaMA
-DATA_PATH=../data
-# DATA_PATH=../data/Beauty
-
-OUTPUT_DIR=./ckpt/$DATASET/
+DATASET=Instruments_copytwo
+BASE_MODEL="LLM path"
+DATA_PATH=./data
+OUTPUT_DIR=./ckpt_Our/$DATASET/
 
 torchrun --nproc_per_node=4 --master_port=3325  lora_finetune.py \
     --base_model $BASE_MODEL\
     --output_dir $OUTPUT_DIR \
     --dataset $DATASET \
     --data_path $DATA_PATH \
-    --per_device_batch_size 16 \
+    --per_device_batch_size 64 \
     --learning_rate 1e-4 \
-    --epochs 4 \
+    --epochs 1 \
     --tasks seqrec \
     --train_prompt_sample_num 1 \
     --train_data_sample_num 0 \
-    --index_file .index.json\
-    --wandb_run_name test\
+    --index_file .item_sid_output.json\
+    --wandb_run_name test_Our\
     --temperature 1.0
